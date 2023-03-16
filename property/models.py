7481cb457aee9a85f5b00ff4077d5a4ecc367a22
Matheus Florentino de Carvalho <matheus.fcarvalho2001@gmail.com>
2023-03-16 17:25:56 -0300

    feat: address

--- property/models.py
+++ property/models.py
@@ -1,21 +1,14 @@
 # property/models.py
 from django.contrib.postgres.fields import ArrayField
-from django.core.validators import RegexValidator
 from django.db import models
+from address.models import Address
 import uuid
 
 
 class Property(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    cep = models.CharField(max_length=8, validators=[
-                           RegexValidator(r'^\d{8}$', 'CEP inválido')])
-    logradouro = models.CharField(max_length=255)
-    cidade = models.CharField(max_length=100)
-    preco = models.PositiveIntegerField
-    estado = models.CharField(max_length=2)
-    numero = models.PositiveIntegerField(blank=True)
+    address = models.OneToOneField(
+        Address, on_delete=models.CASCADE, null=True)
+    price = models.PositiveIntegerField()
     imagens = ArrayField(models.BinaryField(), blank=True, null=True)
     descricao = models.TextField()
-
-
-
